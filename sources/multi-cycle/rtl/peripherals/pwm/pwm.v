57940182e1234043d766448ceb031f438a18a69c
Sebastián García <isebastian.ga@gmail.com>
2023-11-29 15:34:43 -0500

    Update PWM frequency divider counter width

--- sources/multi-cycle/rtl/peripherals/pwm/pwm.v
+++ sources/multi-cycle/rtl/peripherals/pwm/pwm.v
@@ -23,7 +23,7 @@ module pwm (
 
     // Frequency divider
     localparam F_DIV = 1_111;
-    reg [9:0] f_div_counter = 0;
+    reg [10:0] f_div_counter = 0;
     wire f_div_enable;
     assign f_div_enable = (f_div_counter == F_DIV) ? 1 : 0;
 
